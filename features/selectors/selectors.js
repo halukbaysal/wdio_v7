d7113454a2e0bc714721a6c3cccb00e60d5da1fa
halukbaysal <48485249+halukbaysal@users.noreply.github.com>
2021-03-01 15:44:21 -0500

    Create selectors.js

---
+++ features/selectors/selectors.js
@@ -0,0 +1,8 @@
+const selectors = {
+  username: "[data-test='username']",
+  password: "[data-test='password']",
+  loginButton: "#login-button",
+  inventory: "#inventory_container",
+};
+
+module.exports = selectors;
